1d0926edad05e580da63dc14d957f012a2471a8d
PH <mailinpro@gmail.com>
2022-03-18 12:53:11 +0100

    Appliquer des modifications au réseau virtuel "tags"

--- main.tf
+++ main.tf
@@ -25,5 +25,10 @@ resource "azurerm_virtual_network" "vnet" {
   address_space       = ["10.0.0.0/16"]
   location            = "westus2"
   resource_group_name = azurerm_resource_group.rg.name
-}
 
+  # Modification of the existing resource
+  tags = {
+    Environment = "Terraform Getting Started"
+    Team        = "DevOps"
+  }
+}
