791ced87a768c4aef12d7e28d54637f1b2ec1fd3
PH <mailinpro@gmail.com>
2022-03-18 12:43:56 +0100

    Création d'un réseau virtuel dans le main.tf

--- main.tf
+++ main.tf
@@ -18,3 +18,12 @@ resource "azurerm_resource_group" "rg" {
   name     = "myTFResourceGroup"
   location = "West Europe"
 }
+
+# Create a virtual network 
+resource "azurerm_virtual_network" "vnet" {
+  name                = "myTFVnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = "westus2"
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
